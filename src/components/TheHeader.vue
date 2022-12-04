d3736c3cbe458f6d518b03d635724ede3d1681d9
HelgaToshkollari <toshkollari.helga@gmail.com>
2022-12-04 19:07:51 +0100

    adds nav-bar

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,12 +1,70 @@
 <template>
-    <div class="my-bgc-white">
-        <h1>
-            nav-bar
-        </h1>
+    <div class="container my-bgc-white">
+        <div class="my-nav-bar">
+            <div class="logo-dc">
+                <img src="/img/dc-logo.png" alt="">
+            </div>
+            <div class="nav-bar-links">
+                <ul class="links">
+                    <li v-for="links in navbarLinks"><a href="#"> <strong>{{links.link}}</strong></a></li>
+                </ul>
+
+            </div>
+
+        </div>
+
+        
     </div>
 </template>
 <script>
 export default {
+    data(){
+        return{
+            navbarLinks: [
+                {
+                    link: "CHARACTERS"
+
+                },
+                {
+                    link: "COMICS"
+
+                },
+                {
+                    link: "MOVIES"
+
+                },
+                {
+                    link: "TV"
+
+                },
+                {
+                    link: "GAMES"
+
+                },
+                {
+                    link: "COLLECTIBLES"
+
+                },
+                {
+                    link: "VIDEOS"
+
+                },
+                {
+                    link: "FANS"
+
+                },
+                {
+                    link: "NEWS"
+
+                },
+                {
+                    link: "SHOP"
+
+                },
+
+            ],
+        }
+    }
     
 }
 </script>
@@ -15,6 +73,23 @@ export default {
 .my-bgc-white{
     background-color:$my-bgc-color-header;
     
+}
+.my-nav-bar{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+}
+.links{
+    list-style: none;
+    display: flex;
+
+    a{
+        color: $my-bgc-color-dark;
+        text-decoration: none;
+        padding: 10px;
+         
+    }
 }
     
 </style>
